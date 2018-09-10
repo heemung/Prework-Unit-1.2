2eb79d1cf1ab1515525e97f214ff7d7aa38798ca
Clayton <clayton.cox@gmail.com>
2018-09-10 19:38:01 -0400

    Program Finished. More testing needed.

--- Prework Unit 1.2/Program.cs
+++ Prework Unit 1.2/Program.cs
@@ -18,40 +18,48 @@ namespace Prework_Unit_1._2
         {
             Console.WriteLine("This Program will calculate the difference between two dates\n");
 
+            string userDate1 = "0", userDate2 = "0";
             bool tempLoop = true;
             TimeSpan difference;
 
-            while (temp)
+            while (tempLoop)
             {
-                Console.WriteLine("Please enter the first date\n");
-                string userDate1 = Console.ReadLine();
+                Console.WriteLine("Please enter the first date in format '1/11/2011' : ");
+                userDate1 = Console.ReadLine();
 
-                if(testDate == false)
-                    tempLoop = 
+                if (TestDate(userDate1) == true)
+                    tempLoop = false;
             }
 
-            Console.WriteLine("Please enter the first date\n");
-            string userDate2 = Console.ReadLine();
+            tempLoop = true;
+            while (tempLoop)
+            {
+                Console.WriteLine("Please enter the second in format '1/11/2011' :");
+                userDate2 = Console.ReadLine();
 
-            //Console.WriteLine("Enter the Month ");
-            string dateString = "02/30/2008";
+                if (TestDate(userDate2) == true)
+                    tempLoop = false;
+            }
+
 
             //DateTime date1 = DateTime.Parse(dateString,
-                         //             System.Globalization.CultureInfo.InvariantCulture);
+            //             System.Globalization.CultureInfo.InvariantCulture);
             //Console.WriteLine(date1);
 
             // Define two dates. year, month, day, military hour, min, sec
-            DateTime date2 = new DateTime(2010, 1, 1, 8, 0, 15);
-            DateTime date3 = new DateTime(2010, 8, 18, 13, 30, 30);
+            DateTime date1 = DateTime.Parse(userDate1,
+                System.Globalization.CultureInfo.InvariantCulture);
+            DateTime date2 = DateTime.Parse(userDate2,
+                System.Globalization.CultureInfo.InvariantCulture);
 
             // Calculate the interval between the two dates without negitive numbers.
-            if (date3 >= date2)
+            if (date2 >= date1)
             {
-                difference = date3 - date2;
+                difference = date2 - date1;
             }
             else
             {
-                difference = date2 - date3;
+                difference = date1 - date2;
             }
             //test for the timespan answer
             //Console.WriteLine("{0} - {1} = {2}", date2, date3, difference.ToString());
@@ -59,12 +67,15 @@ namespace Prework_Unit_1._2
 
             Console.WriteLine("{0} {1} {2} {3} {4} {5} {6}", "The difference is: ",
                  difference.Days, "days,", difference.Hours, "hours and", difference.Minutes, 
-                 "mins");
+                 "mins \n OR \n" );
+            Console.WriteLine("{0} {1} {2} {3} {4} {5} {6}", "The difference is: ",
+                difference.TotalDays, "Total days. OR", difference.TotalHours, "Total hours. OR", 
+                difference.TotalMinutes,"Total mins.");
 
             Console.ReadLine();
         }
 
-        public static bool testDate (string inputDateTogther)
+        public static bool TestDate (string inputDateTogther)
         {
             DateTime tempDate;
             if (DateTime.TryParse(inputDateTogther, out tempDate))
